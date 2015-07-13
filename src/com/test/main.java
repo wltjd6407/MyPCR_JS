d257e56a92c91fa6ec20480da7e2322361c60575
wltjd6407 <wltjd6407@naver.com>
2015-07-13 15:39:54 +0900

    Hello World

--- src/com/test/main.java
+++ src/com/test/main.java
@@ -1,13 +1,10 @@
 package com.test;
 
-public class main {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class main 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
 	}
 
 }
